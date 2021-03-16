4ee60488f820a0bbe933c7c563744f9bd40bc615
Siddhant Kumar <siddhantkumar052@gmail.com>
2021-03-16 13:18:48 +0530

    console

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 
+console.log(txtInput)
 
-btnTranslate.addEventListener( "click", function clickEventHandler(){
-    console.log("clicked")
-}
+function clickEventHandler() {
+    console.log("clicked");
+    console.log("input",txtInput.value)
+};
 
-)
+btnTranslate.addEventListener( "click",  clickEventHandler)
+    
